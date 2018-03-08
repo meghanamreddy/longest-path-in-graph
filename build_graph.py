bd518815515c769e834b289308d005a7bced2817
Meghana M Reddy <36499567+meghanamreddy@users.noreply.github.com>
2018-03-08 19:58:36 +0530

    Code updated

--- build_graph.py
+++ build_graph.py
@@ -16,6 +16,7 @@ def buildGraph(wordFile):
         word = line[:-1].lower()
         graph[line[:-1]] = []
 
+    # Add every work to its corresponding bucket
     for word in graph:    
         for i in range(len(word)):
             bucket = word[:i] + '_' + word[i+1:]
@@ -28,12 +29,15 @@ def buildGraph(wordFile):
             if del_word in graph:
                 m[word] = del_word
     
+    # sort the buckets
     for k in d:
         d[k].sort()
         for i in range(0, len(d[k])):
             for j in range(i, len(d[k])):
                 if d[k][i]!=d[k][j]:
                     graph[d[k][i]].append(d[k][j])
+                    
+    # build the graph based on the sorted buckets
     for k in m:
         if(m[k] < k):
             graph[m[k]].append(k)
